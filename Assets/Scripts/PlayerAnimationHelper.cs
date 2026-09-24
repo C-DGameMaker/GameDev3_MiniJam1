f1bf411747ff6d210416ee423790c3f28b3151ec
Holliday <mackeybenjamin1@gmail.com>
2026-09-24 17:58:34 -0300

    cleanup

--- Assets/Scripts/PlayerAnimationHelper.cs
+++ Assets/Scripts/PlayerAnimationHelper.cs
@@ -5,7 +5,42 @@ public class PlayerAnimationHelper : MonoBehaviour
 
 
     [SerializeField] Animator playerAnimator;
+    [SerializeField] GameObject player;
+    private PlayerInputs inputs;
+    private PlayerMotor motor;
+
     int isMovingHash = Animator.StringToHash("isMoving");
+
+    //==================READABLE VALUES=====================) (im not sure how this works, feel free to assign values from here!)
+    bool isMoving;
+    bool isGrounded;
+
+    //extras
+    float movmentSpeed;
+    float movmentDirection;
+    //=======================================================
+
+    void Start()
+    {
+        if(player == null)
+        {
+            Debug.LogError("No player given to the animation helper!");
+            return;
+        }
+
+        inputs = player.GetComponent<PlayerInputs>();
+    }
+
+    void UpdateValues()
+    {
+        movmentSpeed = motor.momentum.x;
+
+        if(movmentSpeed <= 0.001f) isMoving = false;
+        else isMoving = true;
+
+        movmentDirection = inputs.movmentDirection;
+        isGrounded = inputs.grounded;
+    }
     void AnimationMovement()
     {
         //if(true)
@@ -20,6 +55,7 @@ public class PlayerAnimationHelper : MonoBehaviour
 
     private void Update()
     {
+        UpdateValues();
         AnimationMovement();
     }
 }
