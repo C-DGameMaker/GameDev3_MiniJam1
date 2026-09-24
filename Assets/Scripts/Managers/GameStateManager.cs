2ec725b5f207c9b1a51b3c9f1dc2a82f0a964466
C-DGameMaker <charlotte5634@outlook.com>
2026-09-24 13:36:39 -0300

    Started commenting my code

--- Assets/Scripts/Managers/GameStateManager.cs
+++ Assets/Scripts/Managers/GameStateManager.cs
@@ -2,32 +2,45 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 
+/// <summary>
+/// Charlie Dobson 
+/// 
+/// Sets the current Game state within the game
+/// </summary>
+
+
+// An enum of all the GameStates
 public enum GameStates
 {
-    init,
-    MainMenu,
-    Gameplay,
-    Paused,
-    Death
+    init, //Intialize
+    MainMenu, //Main Menu
+    Gameplay, //Gameplay
+    Paused, // Paused
+    Death //Death
 
 }
 
-
 public class GameStateManager : MonoBehaviour
 {
-    [SerializeField] EventSystem eventSystem; 
+    [SerializeField] EventSystem eventSystem;
+
+    // To show what the gameStates are, in both string form and GameState form
+    // to change them. 
     [SerializeField] private string _currentActiveState;
     [SerializeField] private string _previousActiveState;
     public GameStates _currentState { get; set; }
     public GameStates _previousState { get; set; }
 
+    // The service hub manager
     public ServiceHubManager _manager;
 
     private void Start()
     {
+        // Sets the state to intialize
         SetState(newState: GameStates.init);
     }
 
+    // Changes the current state of the 
     public void SetState(GameStates newState)
     {
         _previousState = _currentState;
