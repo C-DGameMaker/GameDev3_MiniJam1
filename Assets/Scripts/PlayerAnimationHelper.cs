5eacec08aa6848fafeee591cedd88e99bdec0780
Max <maxbruce04@hotmail.com>
2026-09-26 15:13:37 -0300

    animated troubleshooting

--- Assets/Scripts/PlayerAnimationHelper.cs
+++ Assets/Scripts/PlayerAnimationHelper.cs
@@ -2,15 +2,64 @@ using UnityEngine;
 
 public class PlayerAnimationHelper : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+
+    [SerializeField] Animator playerAnimator;
+    private GameObject player;
+    private PlayerInputs inputs;
+    [SerializeField] PlayerMotor motor;
+
+    int isMovingHash = Animator.StringToHash("isMoving");
+    int isGroundedHash = Animator.StringToHash("isGrounded");
+
+    //==================READABLE VALUES=====================) (im not sure how this works, feel free to assign values from here!)
+    bool isMoving;
+    bool isGrounded;
+
+    //extras
+    float movmentSpeed;
+    float movmentDirection;
+    //=======================================================
+
     void Start()
     {
-        
+        player = this.gameObject;
+        if (player == null)
+        {
+            Debug.LogError("No player given to the animation helper!");
+            return;
+        }
+        inputs = player.GetComponent<PlayerInputs>();
+        motor = player.GetComponent<PlayerMotor>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateValues()
     {
-        
+        movmentSpeed = motor.momentum.x;
+        Debug.Log("Speed: " + movmentSpeed);
+        Debug.Log(motor.momentum.x);
+
+        if (movmentSpeed <= 0.001f) isMoving = false;
+        else isMoving = true;
+
+        movmentDirection = inputs.movmentDirection;
+        isGrounded = inputs.grounded;
+    }
+    void AnimationMovement()
+    {
+        if (isMoving == true && isGrounded == true)
+        {
+            playerAnimator.SetBool(isMovingHash, true);
+        }
+        else
+        {
+            playerAnimator.SetBool(isMovingHash, false);
+        }
+    }
+
+    private void Update()
+    {
+        UpdateValues();
+        AnimationMovement();
     }
 }
